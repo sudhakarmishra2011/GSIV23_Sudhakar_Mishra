c1a284f8a3cbf24330c3aedfd991b6c4ab09d88a
Sudhakar <sudhakarmishra2011@gmail.com>
2023-08-15 17:32:02 +0530

    Updated Card Component with Ellipses

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,21 +2,24 @@ import React from 'react'
 import './style.css'
 import logo from '../../assets/spiderman.jpg'
 
-const Card = (props) => {
+const Card = ({data}) => {
+    console.log(data)
+    let url = "https://image.tmdb.org/t/p/original";
+
   return (
    <div className="cardContainer">
-    <div className="cardElement">
+    {data && data.map && data.map((dt)=>
+        <div className="cardElement" key={dt.id}>
         <div className="movieImage">
-        <img src={logo} height={150} alt='img'/>
+        <img src={url + dt.poster_path} height={150} alt='img'/>
         </div>
         <div className="movieDetails">
-            <div className="movieTitle">Movie Title</div>
-            <div className="rating">(Rating)</div>
+            <div className="movieTitle">{dt.title}</div>
+            <div className="rating">{dt.vote_average.toFixed(1)}/10</div>
         </div>
-        <div className="description">
-        Description
-        </div>
+        <div className="description">{dt.overview}</div>
     </div>
+    )}
    </div>
   )
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import './style.css'
 import Card from '../Card/Card'
+import { useSelector } from 'react-redux'
 
 const Home = () => {
+
+  const { data } = useSelector((state) => state.home)
+
   return (
     <div className="HomeContainer">
-        <Card />
+        <Card data={data}/>
     </div>
   )
 }
