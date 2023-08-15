f8afc6e6004b863b8b0431af9e20d3441974a4c1
Sudhakar <sudhakarmishra2011@gmail.com>
2023-08-15 14:02:26 +0530

    Added Header Component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import './style.css'
+import homeIcon from '../../assets/home.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='headerContainer'>
+      <div className="searchContainer">
+        <input type='text' className="icon" placeholder='Search'/>
+      </div>
+      <div className="homeIconContainer">
+        <img src={homeIcon} alt='homeIcon' className='homeIcon'/>
+      </div>
+    </div>
   )
 }
 
